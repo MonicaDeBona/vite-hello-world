7106365df2cac4121b8d39e8d87201f1d4cfdece
MonicaDeBona <debona.monica@outlook.it>
2022-12-23 17:35:00 +0100

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script>
-import { def } from '@vue/shared';
 import AppTitle from './components/AppTitle.vue';
 export default {
   components: {
@@ -13,15 +12,7 @@ export default {
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  h1 {
+    color: rgb(247, 8, 139);
+  }
 </style>
